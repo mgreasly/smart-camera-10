4e4102471a3ad4abcc95007234d7e72e1c5c7c4c
Marcus Greasly <marcus.greasly@avanade.com>
2018-05-02 11:20:14 +1000

    Updated API call

--- components/store.js
+++ components/store.js
@@ -8,32 +8,29 @@ const mapToProps = ({ results, deviceId }) => ({ results, deviceId });
 const actions = ({ setState }) => ({
     getResults(state, value) {
         return axios.post(
-            'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyCTVMHDJUxUdkd9_0NhrKGC-86PObf9QYM',
-            {
-                "requests":[{
-                    "image":{ "content": value.replace("data:image/jpeg;base64,", "") },
-                    "features":[ { "type":"LOGO_DETECTION", "maxResults": 1 } ]
-                }]
-            }
+            'http://workshop-ava.azurewebsites.net/api/Camera/RecognizeImage', 
+            value
         )
         .then(response => {
-            var product = {
-                name: response.data.responses[0].logoAnnotations[0].description,
-                description: '',
-                price: ''
+            var data = JSON.parse(response.data);
+            var product = data.Products[0];
+            var result = {
+                name: product.Name,
+                description: product.Products[0].FullDescription,
+                price: product.Products[0].Price
             };
             var results = state.results.concat([{
                 image: value,
-                product: product
+                product: result
             }]);
-            return { results: results, deviceId: state.deviceId }
+            return { results: results }
         })
         .catch(error => {
             var results = state.results.concat([{
                 image: value,
-                product: { name: 'unidentified', description: '', price: '' }
+                product: null
             }]);
-            return { results: results, deviceId: state.deviceId }
+            return { results: results }
         })
     },
     setDeviceId(state, value) { return { results: state.results, deviceId: value } }    
